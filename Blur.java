8ac4409bd237d8454aa58447d30dc6df42943eb0
ShunsukeSano <s1531029@eng.kitakyu-u.ac.jp>
2014-04-27 21:06:27 +0900

    Blur
    画像にぼかしを入れるプログラム
    Blur.java7行目でぼかしの強さを変更可能

---
+++ Blur.java
@@ -0,0 +1,51 @@
+﻿import static img.ImageManager.*;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+public class Blur {
+    public static final int RANGE = 10;
+    public static void main(String[] args) throws IOException {
+        File file_read = new File("amane.jpg");
+        BufferedImage read = ImageIO.read(file_read);                       //画像の読み込み
+        int width = read.getWidth(), height = read.getHeight();             //画像のサイズを取得
+        BufferedImage write =
+                new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        
+        int average_count = 0;
+        for(int y = 0; y < height; y++){                                    //全てのピクセルを見る
+            for(int x = 0; x < width; x++){
+                int r = 0;
+                int g = 0;
+                int b = 0;
+                
+                for(int i = -RANGE; i < RANGE+1; i++){                      //周囲10*10マスのRGBを平均
+                    for(int j = -RANGE; j < RANGE+1; j++){
+                        if(y+i >= 0 && y+i < height && x+j >= 0 && x+j < width){
+                            int c = read.getRGB(x+j, y+i);
+                            r += r(c);
+                            g += g(c);
+                            b += b(c);
+                            average_count++;
+                        }
+                    }
+                }
+
+                int average_r = r / average_count;
+                int average_g = g / average_count;
+                int average_b = b / average_count;
+                average_count = 0;
+
+                int c_center = read.getRGB(x, y);
+                r = (r(c_center) + average_r*99)/100;                       //対象のピクセルとその周りのピクセルどちらに比重をおくか、またその比
+                g = (g(c_center) + average_g*99)/100;
+                b = (b(c_center) + average_b*99)/100;
+
+                int rgb = rgb(r,g,b);
+                write.setRGB(x,y,rgb);
+            }
+        }
+        File file_write = new File("RANGE10jpg");
+        ImageIO.write(write, "jpg", file_write);
+    }
+}
---
+++ img/ImageManager.java
@@ -0,0 +1,40 @@
+﻿package img;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+
+public class ImageManager{
+    static BufferedImage reader;
+    static BufferedImage writer;
+    static int width;
+    static int height;
+
+    public static void set_image(String file_name) throws IOException{
+        reader = ImageIO.read(new File(file_name));
+        width = reader.getWidth();
+        height = reader.getHeight();
+        writer = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    }
+
+    public static int a(int c){
+        return c>>>24;
+    }
+    public static int r(int c){
+        return c>>16&0xff;
+    }
+    public static int g(int c){
+        return c>>8&0xff;
+    }
+    public static int b(int c){
+        return c&0xff;
+    }
+    public static int rgb
+    (int r,int g,int b){
+        return 0xff000000 | r <<16 | g <<8 | b;
+    }
+    public static int argb
+    (int a,int r,int g,int b){
+        return a<<24 | r <<16 | g <<8 | b;
+    }
+
+}
